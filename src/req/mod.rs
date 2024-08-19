bdd65dca273b3f17d2206095dcfebaa3f9d5d02d
llanero <llanerolsl@qq.com>
2024-08-19 23:06:05 +0800

    add qb client

--- src/req/mod.rs
+++ src/req/mod.rs
@@ -1,29 +1,85 @@
+use std::sync::OnceLock;
+
+use anyhow::{anyhow, Error, Result};
+use qbit_rs::{
+    model::{Credential, GetTorrentListArg, Torrent},
+    Qbit,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct User {
+pub struct User {
+    url: String,
     username: String,
     password: String,
 }
 
+static CLIENT: OnceLock<Qbit> = OnceLock::new();
+
+pub async fn qb_login(user: User) -> anyhow::Result<()> {
+    let credential = Credential::new(user.username, user.password);
+
+    let qbit = Qbit::builder()
+        .endpoint(&(*user.url))
+        .credential(credential)
+        .build();
+
+    if let Ok(()) = CLIENT.set(qbit) {
+        Ok(())
+    } else {
+        Err(anyhow!("the client logined repeatlly"))
+    }
+}
+
+pub async fn get_torrents() -> anyhow::Result<Vec<Torrent>> {
+    let client = CLIENT.get();
+
+    if let Some(qbit) = client {
+        let arg = GetTorrentListArg::builder().build();
+
+        qbit.get_torrent_list(arg)
+            .await
+            .map_err( anyhow::Error::new)
+
+    } else {
+        Err(anyhow!("the client is not inited."))
+    }
+}
+
 #[cfg(test)]
 mod qb_req {
-    use std::error::Error;
+    use crate::req::{get_torrents, User};
+    use std::{error::Error, sync::Arc};
 
     use qbit_rs::{model::Credential, Qbit};
     use reqwest::blocking::Client;
+    use tokio::sync::RwLock;
+
+    #[tokio::test]
+    async fn rwlock_test() {
+        let lock = Arc::new(RwLock::new(6));
+
+        let lock_clone = lock.clone();
+
+        let handle = tokio::spawn(async move {
+            let read_guard = lock_clone.read().await;
+            println!("Read: {}", *read_guard);
+        });
+
+        handle.await.unwrap();
+    }
 
     #[test]
     fn get_req() -> Result<(), Box<dyn Error>> {
         let client = Client::new();
 
-        let url = String::from("https://www.qb.com/api/v2/auth/login");
+        let url = String::from("https://xxx.qb_user.com");
 
-        let params = [("username", "username"), ("password", "password")];
+        let params = [("username", "qb_user"), ("password", "password_qb")];
 
         let response = client
             .post(url)
-            .header("Referer", "https://www.qb.com")
+            .header("Referer", "https://xxx.qb_user.com")
             .form(&params)
             .send()?;
 
@@ -32,14 +88,14 @@ mod qb_req {
             let body = response.text()?;
             println!("res: {}, headers: {:?}", body, headers);
         } else {
-            println!("fail status: {}", response.status());
+            eprintln!("fail status: {}", response.status());
         }
         Ok(())
     }
 
     #[tokio::test]
     async fn qb_login() {
-        let credential = Credential::new("username", "password");
+        let credential = Credential::new("qb_user", "password_qb");
         let api = Qbit::builder()
             .endpoint("https://xxx.xxx.xxx")
             .credential(credential)
@@ -55,6 +111,24 @@ mod qb_req {
 
         if let Ok(torrents) = api.get_categories().await {
             println!("category {:?}", torrents);
+        }
+    }
+
+    #[tokio::test]
+    async fn qb_login_test() {
+        let user = User {
+            url: "https://xxx.qb_user.com".to_string(),
+            username: "qb_user".to_string(),
+            password: "password_qb".to_string(),
+        };
+
+        if let Err(error) = super::qb_login(user).await {
+            eprintln!("error login {}", error)
+        }
+
+        match get_torrents().await {
+            Ok(version) => println!("version:{:?}", version[0]),
+            Err(error) => eprintln!("get version failed, {}", error),
         }
     }
 }
