4cdbe3534799df7b818a85cacff80388b8f76c5b
llanero <llanerolsl@qq.com>
2025-01-23 21:13:22 +0800

    temp commit

--- src/meta/mod.rs
+++ src/meta/mod.rs
@@ -6,17 +6,21 @@ use std::{
     path::Path,
 };
 
-struct Similar {
+use serde::Serialize;
+
+use crate::req::{self, Torrent};
+
+#[derive(Debug, Serialize)]
+pub(crate) struct Similar {
     inode: u64,
     source_dir: String,
     dest_dir: String,
     link_type: LinkType,
 }
 
-#[derive(Debug)]
-enum LinkType {
-    FILE,
-    DIRECTORY,
+#[derive(Debug, Serialize)]
+pub(crate) enum LinkType {
+    File,
 }
 
 /// get the each file's inode and absolute path of dir recursively, keep them in param map
@@ -36,6 +40,97 @@ fn get_inodes(dir: &Path, map: &mut HashMap<u64, String>) -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug)]
+pub(crate) struct MappringReleation {
+    manager_dir: String,
+    client_dir: String,
+}
+
+/// deal releations between the torrent client's save_dir and the manager's dir, and save the
+/// releation of save_dir and torrent
+fn deal_torrent_path(
+    torrents: Vec<Torrent>,
+    map_dirs: HashMap<String, Vec<MappringReleation>>,
+) -> HashMap<String, Torrent> {
+    let mut dir_torrent = HashMap::new();
+    for mut torrent in torrents {
+        if let Some(torrent_save_dir) = &torrent.file {
+            if let Some(mapping_relations) = map_dirs.get(&torrent.source) {
+                let releation = mapping_relations
+                    .iter()
+                    .filter(|releation| releation.client_dir.starts_with(torrent_save_dir))
+                    .last();
+
+                if let Some(releation) = releation {
+                    torrent.file = Some(
+                        torrent_save_dir.replace(&releation.client_dir, &releation.manager_dir),
+                    );
+                }
+            }
+
+            let file = torrent.file.as_ref().unwrap().clone();
+
+            dir_torrent.insert(file, torrent);
+        }
+    }
+
+    dir_torrent
+}
+
+#[derive(Debug)]
+enum SeedDir {
+    ALL,
+    PARTIRAL,
+    NONE
+}
+
+/// recursively traverses the specfied folder to get all files and folders, and judge the file
+/// or folder is seeding.
+fn get_unseed_files(
+    torrent_dir_map: &mut HashMap<String, Torrent>,
+    torrent_dir: &Path,
+    unseeding_dirs: &mut Vec<&Path>,
+) -> anyhow::Result<SeedDir> {
+    let base_dir = torrent_dir.to_str().unwrap();
+
+
+    if let Some(torrent) = torrent_dir_map.remove(base_dir) {
+        return Ok(SeedDir::NONE);
+    }
+    // 遍历:
+    if torrent_dir.is_dir() {
+        let has_seeding = false;
+        for enrty in fs::read_dir(torrent_dir)? {
+           has_seeding = get_unseed_files(torrent_dir_map, &enrty?.path(), &mut unseeding_dirs)?;  
+        }
+        if get_unseed_files(, torrent_folders, unseeding_torrents) {
+            unseeding_dirs.push(torrent_dir);
+        }
+    }
+    Ok(SeedDir::NONE)
+}
+
+#[allow(unused)]
+pub async fn list_unseed_files(
+    torrent_folders: Vec<&Path>,
+    map_dirs: HashMap<String, Vec<MappringReleation>>,
+) -> anyhow::Result<Vec<Torrent>> {
+    let mut torrents = req::get_torrents().await?;
+
+    // it has more than one torrent in the same dir, but it's no problem
+
+    let mut torrent_dir_map = deal_torrent_path(torrents, map_dirs);
+
+    let mut unseeding_torrents = Vec::new();
+
+    get_unseed_files(
+        &mut torrent_dir_map,
+        &torrent_folders,
+        &mut unseeding_torrents,
+    );
+
+    Ok(unseeding_torrents)
+}
 
 /// Read all files in the source path and destination directory to obtain the relationship between all hard-connected files.
 /// The key is the inode of the file, and the value is the tuple composed of the absolute path of the source file and the destination file   
@@ -54,7 +149,7 @@ pub fn get_linked_files(source_dir: &Path, dest_dir: &Path) -> Result<HashMap<u6
                 inode: ino,
                 source_dir: source_path,
                 dest_dir: dest_path,
-                link_type: LinkType::FILE,
+                link_type: LinkType::File,
             };
             map.insert(ino, simalir);
         }
--- src/req/mod.rs
+++ src/req/mod.rs
@@ -1,11 +1,18 @@
-use std::sync::OnceLock;
+use std::{borrow::Borrow, sync::OnceLock};
 
-use anyhow::{anyhow, Error, Result};
+use anyhow::anyhow;
+use lazy_static::lazy_static;
 use qbit_rs::{
-    model::{Credential, GetTorrentListArg, Torrent},
+    model::{Credential, GetTorrentListArg},
     Qbit,
 };
+use reqwest::Url;
 use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
+use transmission_rpc::{
+    types::{BasicAuth, ErrorType},
+    TransClient,
+};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
@@ -14,8 +21,43 @@ pub struct User {
     password: String,
 }
 
-static CLIENT: OnceLock<Qbit> = OnceLock::new();
+#[allow(unused)]
+static QB_CLIENT: OnceLock<Qbit> = OnceLock::new();
 
+//static TR_CLIENT: OnceLock<TransClient> = OnceLock::new();
+
+lazy_static! {
+    static ref TR_CLIENT: Mutex<Option<TransClient>> = Mutex::new(None);
+}
+
+#[allow(unused)]
+pub async fn transmission_login(user: User) -> anyhow::Result<()> {
+    let basic_auth = BasicAuth {
+        user: user.username,
+        password: user.password,
+    };
+
+    let client = TransClient::with_auth((&(*user.url)).try_into()?, basic_auth);
+
+    let mut tr_client_gurad = TR_CLIENT.lock().await;
+    *tr_client_gurad = Some(client);
+
+    Ok(())
+}
+
+#[allow(unused)]
+pub async fn tran_login_barely(url_str: &str) -> anyhow::Result<()> {
+    let url: Url = url_str.try_into()?;
+
+    let client = TransClient::new(url);
+
+    let mut tr_client_guard = TR_CLIENT.lock().await;
+    *tr_client_guard = Some(client);
+
+    Ok(())
+}
+
+#[allow(unused)]
 pub async fn qb_login(user: User) -> anyhow::Result<()> {
     let credential = Credential::new(user.username, user.password);
 
@@ -24,25 +66,89 @@ pub async fn qb_login(user: User) -> anyhow::Result<()> {
         .credential(credential)
         .build();
 
-    if let Ok(()) = CLIENT.set(qbit) {
+    if let Ok(()) = QB_CLIENT.set(qbit) {
         Ok(())
     } else {
         Err(anyhow!("the client logined repeatlly"))
     }
 }
 
-pub async fn get_torrents() -> anyhow::Result<Vec<Torrent>> {
-    let client = CLIENT.get();
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Torrent {
+    pub file: Option<String>,
+    pub source: String,
+}
 
-    if let Some(qbit) = client {
+#[allow(unused)]
+/// todo
+/// get the torrent list from qbittorrent and tranmission
+pub async fn get_torrents() -> anyhow::Result<Vec<Torrent>> {
+    let qb_client = QB_CLIENT.get();
+
+    if let Some(qbit) = qb_client {
         let arg = GetTorrentListArg::builder().build();
 
-        qbit.get_torrent_list(arg)
+        let qb_torrents: Vec<Torrent> = qbit
+            .get_torrent_list(arg)
             .await
-            .map_err( anyhow::Error::new)
+            .map_err(anyhow::Error::new)?
+            .into_iter()
+            .map(|torrent| Torrent {
+                file: torrent.save_path,
+                source: "qb".to_string(),
+            })
+            .collect();
+    }
 
+    if let Some(ref mut tr_client) = *TR_CLIENT.lock().await {
+        let tr_torrents: Vec<Torrent> = tr_client
+            .torrent_get(None, None)
+            .await
+            .map(|res| res.arguments.torrents)
+            .map_err(|e| anyhow!(e))?
+            .into_iter()
+            .map(|torrent| -> Torrent {
+                Torrent {
+                    file: torrent.download_dir,
+                    source: "tr".to_string(),
+                }
+            })
+            .collect();
+    }
+
+    Ok(Vec::new())
+}
+
+/// Query the seeds that match the given information among the seeds whose status is Error or Alarm
+#[allow(unused)]
+pub async fn match_torrent_info(
+    info: &str,
+) -> Result<Vec<transmission_rpc::types::Torrent>, anyhow::Error> {
+    let mut tr_gurad = TR_CLIENT.lock().await;
+
+    if let Some(ref mut tr_client) = *tr_gurad {
+        let res_torrents: Vec<transmission_rpc::types::Torrent> = tr_client
+            .torrent_get(None, None)
+            .await
+            .map(|res| res.arguments.torrents)
+            .map_err(|e| anyhow!(e))?
+            .into_iter()
+            .filter(|torrent| {
+                let is_ok = torrent.error.unwrap_or(ErrorType::Ok) == ErrorType::Ok;
+                if is_ok {
+                    return false;
+                }
+                torrent
+                    .error_string
+                    .borrow()
+                    .as_deref()
+                    .unwrap_or("")
+                    .contains(info)
+            })
+            .collect();
+        Ok(res_torrents)
     } else {
-        Err(anyhow!("the client is not inited."))
+        Err(anyhow!("tranmission client is not exists"))
     }
 }
 
@@ -54,6 +160,25 @@ mod qb_req {
     use qbit_rs::{model::Credential, Qbit};
     use reqwest::blocking::Client;
     use tokio::sync::RwLock;
+
+    #[tokio::test]
+    async fn match_info_test() {
+        let url = "http://192.168.31.151:19091/transmission/rpc";
+        let error_info = "Ensure your drives are connected or use";
+        match super::tran_login_barely(url).await {
+            Ok(_) => match super::match_torrent_info(error_info).await {
+                Ok(torrents) => {
+                    println!("the torrents:{:?}", torrents);
+                }
+                Err(e) => {
+                    println!("no matched torrents, {}", e);
+                }
+            },
+            Err(e) => {
+                eprintln!("login failed, {}", e);
+            }
+        }
+    }
 
     #[tokio::test]
     async fn rwlock_test() {
